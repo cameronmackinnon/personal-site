3554ba7cc204fc747a2cffacf26491a9d48d3678
Cameron MacKinnon <cameronmackinn@gmail.com>
2024-01-15 13:27:49 -0500

    trying other git users solution

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,12 +15,10 @@ module.exports = {
   plugins: ['react'],
   rules: {
     'jsx-a11y/anchor-is-valid': ['error', {
-      'components': ['Link'],
-      'specialLink': ['to', 'hrefLeft', 'hrefRight'],
-      'aspects': ['noHref', 'invalidHref', 'preferButton'],
-      'linebreak-style': 0,
-      'global-require': 0,
-      'eslint linebreak-style': [0, 'error', 'windows'],
+      aspects: ['noHref', 'invalidHref', 'preferButton'],
+      components: ['Link'],
+      specialLink: ['to', 'hrefLeft', 'hrefRight'],
+      linebreak-style: ['error', 'windows'],
     }],
     'jsx-a11y/click-events-have-key-events': 0,
     'jsx-a11y/href-no-hash': 'off',
